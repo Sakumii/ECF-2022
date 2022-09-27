50dbc7e5ef39bdd66d66494950a6e0608a027d1c
Sakumi <sakumimi10@hotmail.fr>
2022-09-27 17:41:03 +0200

    debug js login btn and main section

--- src/js/login.js
+++ src/js/login.js
@@ -5,34 +5,40 @@ const displayForm = _('displayForm');
             const forRegister = _('forRegister');
             const formRegister = _('registerForm');
             const formContainer = _('formContainer');
-            displayForm.addEventListener('click', showForm)
+            const mainContainer = _('mainContainer');
+            
+            displayForm.addEventListener('click', showForm);
+            displayForm.addEventListener('click', hideMain);
             
             function _(e){
                 return document.getElementById(e);
             }
+            function hideMain() {
+                mainContainer.classList.toggle('hide')
+            };
             function showForm() {
                 document.querySelector('.form_wrapper .card').classList.toggle('show')
             };
-
-            forLogin.addEventListener('click', () =>{
+            forRegister.addEventListener('click', () => {
+                forLogin.classList.remove('active');
+                forRegister.classList.add('active');
+                if(formRegister.classList.contains('toggle_form')) {
+                    formContainer.style.transform = 'translate(-0%)';
+                    formContainer.style.transition = 'transform .5s';
+                    formRegister.classList.remove('toggle_form');
+                    formLogin.classList.add('toggle_form');
+                }
+            });
+                forLogin.addEventListener('click', () =>{
                 forLogin.classList.add('active');
                 forRegister.classList.remove('active');
-                if(forLogin.classList.contains('toggle_form')) {
+                if(formLogin.classList.contains('toggle_form')) {
                     formContainer.style.transform = 'translate(0%)';
                     formContainer.style.transition = 'transform .5s';
                     formRegister.classList.add('toggle_form');
                     formLogin.classList.remove('toggle_form');
             }
         });
+                
 
-            forRegister.addEventListener('click', () => {
-                forLogin.classList.remove('active');
-                forRegister.classList.add('active');
-                if(forRegister.classList.contains('toggle_form')) {
-                    formContainer.style.transform = 'translate(-100%)';
-                    formContainer.style.transition = 'transform .5s';
-                    formRegister.classList.remove('toggle_form');
-                    formLogin.classList.add('toggle_form');
-                }
-            });
-
+S
