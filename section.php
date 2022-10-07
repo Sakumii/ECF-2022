1bb0059f24859da9ffb7c02025dd853b1283d1fe
Sakumi <sakumimi10@hotmail.fr>
2022-10-07 16:13:50 +0200

    .

--- section.php
+++ section.php
@@ -13,7 +13,6 @@
 <main class="columns section_container " id="mainContainer">
         <section class="column is-one-quarter structure_list">
             
-            <h2 class="subtitle is-5">Nombre de structures de la franchise : </h2>
             <h3 class="subtitle is-6">Liste des structures actives : </h3>
                 <ul class="content">
                     <li><p>structure 1</p></li>
